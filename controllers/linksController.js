a700e2bf94b5eef0361173f874638445cdc17a34
Myo-Htet-San-Brainy <myohtetsandrinksgit@gmail.com>
2023-09-18 19:12:39 +0700

    Link Apis done

--- controllers/linksController.js
+++ controllers/linksController.js
@@ -1,22 +1,55 @@
 //packages
+const { StatusCodes } = require("http-status-codes");
 
 //imports
 const Link = require("../models/linksModel");
+const customError = require("../errors");
 
 const getSocialLinks = async (req, res) => {
-  res.send("social links");
+  const links = await Link.find({
+    name: {
+      $ne: "resume",
+    },
+  });
+  const formattedLinks = links.reduce((acc, link) => {
+    acc[link.name] = link.link;
+    return acc;
+  }, {});
+  res.status(StatusCodes.OK).json({ success: true, data: formattedLinks });
 };
 
 const getLink = async (req, res) => {
-  res.send("a link");
+  const { name: linkName } = req.params;
+  const link = await Link.findOne({
+    name: linkName,
+  });
+  if (!link) {
+    throw new customError.NotFound(`No link found with name: ${linkName}`);
+  }
+  res.status(StatusCodes.OK).json({
+    success: true,
+    data: link,
+  });
 };
 
 const updateLink = async (req, res) => {
-  res.send("updated a link");
+  const { name: linkName } = req.params;
+  const link = await Link.findOneAndUpdate(
+    {
+      name: linkName,
+    },
+    req.body,
+    {
+      runValidators: true,
+      new: true,
+    }
+  );
+  res.status(StatusCodes.OK).json({ success: true, data: link });
 };
 
 const createLink = async (req, res) => {
-  res.send("created a link");
+  const link = await Link.create(req.body);
+  res.status(StatusCodes.OK).json({ success: true, data: link });
 };
 
 module.exports = {
